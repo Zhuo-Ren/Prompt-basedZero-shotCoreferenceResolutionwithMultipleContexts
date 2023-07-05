b2be2a355287a5f2dddf1eb6c31b4be7cb19c19d
XuYang <xuyangng_mail@sina.cn>
2023-07-05 21:48:22 +0800

    Evaluation.py version3： Version 2 是给定配置，代码对应的找实验结果。这个版本改为到指定文件夹去读取其下所有实验结果。优化了评估方式。

--- Evaluation/Evaluation.py
+++ Evaluation/Evaluation.py
@@ -2,6 +2,7 @@
 !pip install transformers
 Version 1：这是Evaluation.ipynb的.py版本
 Version 2：在上个版本的基础上删除了下半截的评估代码，只保留了输出F1性能表格的部分。
+Version 3：Version 2 是给定配置，代码对应的找实验结果。这个版本改为到指定文件夹去读取其下所有实验结果。优化了评估方式。
 """
 import warnings
 import ast
@@ -18,6 +19,7 @@ from sklearn.metrics import (
 import torch
 import os
 import csv
+import re
 
 warnings.filterwarnings("ignore")
 
@@ -32,49 +34,32 @@ root_path = local_path
 input_path = f"{root_path}/Results"  # 读取之前各模型的实验结果
 results_path = f"{root_path}/Results/Charts_and_plots/final_plots"  # 生成图表。所以是放在charts_and_plots文件夹下。
 
-# 配置GPU
-"We need to use gpu to compute mention similarity by BERT"
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"device:{device}")
-
-# 配置数据
-data_config = "dev"
 
 # 配置模型类型
 MODEL_LABELS = {  # 总共可选的model: labels
     # "multi-sieves": "Multi-pass Sieve",
     # "e2e": "E2E",
     # "Streamlining": "Streamlining",
-    "GPT2_gold_mentions": "GPT2"
-    # "GPT_NEO-125M_gold_mentions": "GPT-NEO"
-    # "GPT2_WSC": "WSC"
+    "GPT2_gold_mentions": "GPT2",
+    # "GPT_NEO-125M_gold_mentions": "GPT-NEO",
+    # "GPT2_WSC": "WSC",
 }
 models = [  # 总共可选的model
     # "multi-sieves",
     # "e2e",
     # "Streamlining",
-    "GPT2_gold_mentions"
-    # "GPT_NEO-125M_gold_mentions"
-    # "GPT2_WSC"
+    "GPT2_gold_mentions",
+    # "GPT_NEO-125M_gold_mentions",
+    # "GPT2_WSC",
 ]
 labels = [  # 总共可选的labels
     # "Multi-pass Sieve",
     # "E2E",
     # "Streamlining",
     "GPT2",
-    # "GPT-NEO"
-    # "GPT2_WSC"
+    # "GPT-NEO",
+    # "GPT2_WSC",
 ]
-
-# 配置模型参数
-n_shot = 2  # 配置统计数据时，针对GPT-based模型，统计几shot的结果（之前分别按0/2/4/10shot做的实验）
-do_sample = False
-num_beams = 1
-temperature = 1
-
-# 实验参数
-repeat_n = 5
-prefix_source = "ecb"
 
 
 # ####################################################################
@@ -101,99 +86,47 @@ def get_prob(pred, count):
 
 
 def process_prediction_results(
-    model_name, threshold=0.5, reverse=False
+    path, threshold=0.5, reverse=False
 ):
-    dir_name = MODEL_LABELS[model_name]
-    if model_name == "GPT2_WSC":
-        path = os.path.join(
-            input_path, f"{dir_name}/{model_name}_{n_shot}-shots_5-repeats.csv"
-        )
-    else:
-        if do_sample:
-            path = input_path + "/" + f"{dir_name}/{model_name}_{data_config}_{n_shot}shots_{prefix_source}Prefix_{num_beams}beams_doSimple_{temperature}temperature_{repeat_n}repeats.csv"
-        else:
-            path = input_path + "/" + f"{dir_name}/{model_name}_{data_config}_{n_shot}shots_{prefix_source}Prefix_{num_beams}beams_noSimple.csv"
+    """
+    从csv文件中读取信息，计算最终结果。
 
+    - 对每个样例（pred， count）。例如["Prompt 1"]列某个样例预测结果是(3,5)。
+        - 基于每个prompt计算概率
+            - ["prompt_prob 1"]列中概率为0.60（如果count为0则记为np.nan）
+            - ["prompt 1"]列中是1（概率大于等于0.5是1，否则是0）
+        - 基于所有prompt计算众数：["prompt 1"]列到["prompt 5"]列中哪个值出现最多就记为哪个
+        - 基于所有prompt计算均值：["prompt 1"]列到["prompt 5"]列中五个值的均值。
+    - 删除了["text"]列和["mention pair"]列
+    """
     df = pd.read_csv(path)
     df = df[df.label != "label"]  # get rid of header rows
     df.label = df.label.apply(lambda x: int(float(x)))
 
-    # Adds in the predicted probability for each of the prompts, so how many yes (left number) from total (right number)
+    # 添加并计算["prompt_prob n"]列
     for i in range(1, 6):
-        df["pred_prob {}".format(i)] = df["Prompt {}".format(i)].apply(
+        df[f"prompt_prob {i}"] = df[f"Prompt {i}"].apply(
             lambda x: get_prob(ast.literal_eval(x)[0], ast.literal_eval(x)[1])
         )
         if reverse:
-            df["pred_prob {}".format(i)] = 1 - df["pred_prob {}".format(i)]
-
+            df["prompt_prob {}".format(i)] = 1 - df["prompt_prob {}".format(i)]
+    # 添加并计算["prompt n"]列
     for i in range(1, 6):
-        # Adds in boolean yes or no
-        df["pred {}".format(i)] = df["pred_prob {}".format(i)].apply(
+        df[f"prompt {i}"] = df[f"prompt_prob {i}"].apply(
             lambda x: int(x >= threshold)
         )
-
-    majority_columns = [f"pred {i}" for i in range(1, 6)]
-    df["pred majority"] = df[majority_columns].mode(axis=1)[0]
-
-    avg_columns = [f"pred_prob {i}" for i in range(1, 6)]
-    df["pred mean"] = df[avg_columns].mean(axis=1)
-
+    # 添加并计算众数列["prompt majority"]列
+    majority_columns = [f"prompt {i}" for i in range(1, 6)]
+    df["prompt majority"] = df[majority_columns].mode(axis=1)[0]
+    # 添加并计算均值列["prompt mean"]列
+    avg_columns = [f"prompt_prob {i}" for i in range(1, 6)]
+    df["prompt mean prob"] = df[avg_columns].mean(axis=1)
+    df["prompt mean"] = df["prompt mean prob"].apply(
+        lambda x: int(x >= threshold)
+    )
+    # 删除了["text"]列和["mention pair"]列
     df = df.drop(columns=["text", "mention pair"])
     return df
-
-
-def process_prediction_results_nonGPT(model_name, threshold=0.5):
-    if model_name == "multi-sieves":
-        path = os.path.join(input_path, "Multi-pass-Sieve/pairwise_result.csv")
-        df = pd.read_csv(path)
-        df.columns = [
-            "pair",
-            "label",
-            "pred",
-            "mention pair",
-            "sent idx",
-            "sentence",
-            "sent filter",
-            "doc name",
-        ]
-    elif model_name == "e2e":
-        path = os.path.join(input_path, "e2e-coref/pairwise_result.csv")
-        df = pd.read_csv(
-            path,
-            header=None,
-        )
-        df.columns = [
-            "pair",
-            "label",
-            "mention pair",
-            "sent idx",
-            "sentence",
-            "sent filter",
-            "pred",
-            "doc name",
-        ]
-
-    elif model_name == "Streamlining":
-        path = os.path.join(input_path, "Streamlining/pairs_with_mentions.csv")
-        df = pd.read_csv(path)
-
-    else:
-        raise Exception("Invalid model name. Options are 'multi-sieves' or 'e2e'.")
-
-    df = df[df.label != "label"]  # get rid of header rows
-    df.label = df.label.apply(lambda x: int(float(x)))
-
-    if model_name == "Streamlining":
-        df["pred mean"] = 1 * (df["pred_proba"] >= threshold)
-        df["pred majority"] = 1 * (df["pred_proba"] >= threshold)  # df['pred_proba']
-    else:
-        # to make it easier for us to use later scoring functions
-        df.pred = df.pred.apply(lambda x: int(float(x)))
-        df["pred mean"] = df.pred
-        df["pred majority"] = df.pred
-
-    # df = df.drop(columns = ['mention pair',"sent idx","sent filter"])
-    return df[["label", "pred mean", "pred majority"]]
 
 
 # ####################################################################
@@ -212,67 +145,161 @@ METRICS = {
 def get_metrics_scores(
     df,
     metrics=METRICS,
-    aggregate_type="majority",
     threshold=0.5,
 ):
-    true = df["label"].values
-    if aggregate_type == "majority":
-        pred = df["pred majority"].values
-    elif aggregate_type == "mean":
-        pred = (df["pred mean"] >= threshold).astype(int).values
-    else:
-        raise Exception("Invalid aggregate type.")
-    scores = {}
-    for name, scorer in metrics.items():
-        scores[name] = scorer(true, pred)
+    """
+    根据实验结果，计算性能指标。
 
-    return scores
+    return = {
+        "prompt 1": {
+            "acc": 0.723,
+            "precision": 0.531,
+            "recall": 0.613,
+            "f1": 0.551,
+            "auc": 0.333
+        },
+        "prompt 2": {},
+        "prompt 3": {},
+        "prompt 4": {},
+        "prompt 5": {},
+        "prompt mean": {},
+        "prompt majority": {}
+    }
+
+    :param df: df is a pandas DataFrame which has columns includeing "prompt 1", "prompt 2", "prompt 3", "prompt 4", "prompt 5", "prompt majority", "prompt mean"
+    """
+    groundtruth = df["label"].values
+    metrics_of_all_prompt_types = {}
+    prompt_types = ["prompt 1", "prompt 2", "prompt 3", "prompt 4", "prompt 5", "prompt majority", "prompt mean"]
+    for cur_prompt_type in prompt_types:
+        cur_prompt = df[cur_prompt_type].values
+        cur_prompt_metrics = {}
+        for cur_metirc_name, cur_metirc_scorer in metrics.items():
+            cur_prompt_metrics[cur_metirc_name] = cur_metirc_scorer(groundtruth, cur_prompt)
+        metrics_of_all_prompt_types[cur_prompt_type] = cur_prompt_metrics
+    return metrics_of_all_prompt_types
 
 
 # ####################################################################
 # General Evaluation #################################################
 # ####################################################################
+def get_experiment_settings(csv_file_path):
+    csv_file_name = os.path.basename(csv_file_path)
+    groups = re.match(r'([\S]*)_gold_mentions_([\S]*)_shot([\d]{1,2})([\S]*)_beam([\d])_([\S]{2})Sample\(t([\d\.])+r([\d])\).csv', csv_file_name).groups()
+    #
+    r = {}
+    r["model"] = groups[0]
+    r["data"] = groups[1]
+    r["prefix_num"] = groups[2]
+    r["prefix_source"] = groups[3][1:-1] if groups[3] != "" else ""
+    r["beam_size"] = groups[4]
+    r["sample"] = groups[5]
+    r["temperature"] = groups[6]
+    r["repeat"] = groups[7]
+    #
+    return r
 
-def get_score_for_models(model_name, aggregate_type="mean"):
-    if model_name in ["multi-sieves", "e2e", "Streamlining"]:
-        df = process_prediction_results_nonGPT(model_name)
-    elif model_name in ["GPT2_gold_mentions", "GPT_NEO-125M_gold_mentions", "GPT2_WSC"]:
-        df = process_prediction_results(model_name)
-    # elif model_name in ['Streamlining']:
-    #     df = process_prediction_results_streamlining()
-    else:
-        raise Exception("Invalid model name.")
-    scores = get_metrics_scores(df)
-    return scores
+
+def get_score_for_models(input_path):
+    # 获取input_path下的所有csv文件作为输入
+    dir_or_file_list = os.listdir(input_path)
+    csv_file_list = []
+    for dir_or_file in dir_or_file_list:
+        if len(dir_or_file) <= 4:
+            continue
+        if dir_or_file[-4:] != ".csv":
+            continue
+        cur_path = input_path + "/" + dir_or_file
+        if os.path.isfile(cur_path):
+            csv_file_list.append(cur_path)
+    # 遍历所有csv文件
+    prompt_info_list = []
+    for cur_csv_path in csv_file_list:
+        #
+        experiment_settings = get_experiment_settings(cur_csv_path)
+        #
+        experiment_results = process_prediction_results(cur_csv_path)
+        experiment_scores = get_metrics_scores(experiment_results)
+        #
+        for cur_prompt_name, cur_prompt_scores in experiment_scores.items():
+            cur_prompt_info = {"prompt_type": cur_prompt_name}
+            cur_prompt_info.update(cur_prompt_scores)
+            cur_prompt_info.update(experiment_settings)
+            prompt_info_list.append(cur_prompt_info)
+    return prompt_info_list
 
 
-models_performance = {}
-for model in models:
-    scores = get_score_for_models(model, aggregate_type="mean")
-    models_performance[model] = scores
-
-#
-file_path = results_path + f"/{data_config}_models_performance_all.csv"
-csvfile = open(file_path, mode="a", newline='', encoding='utf-8')
-header = ['data', 'model', 'template', 'prefix-source', 'prefix-num', 'do_sample', 'num_beam', 'temperature', 'valid', 'acc', 'auc', 'p', 'r', 'F1']
+prompt_info_list = get_score_for_models(input_path=input_path)
+# =============================================================================
+file_path = results_path + "/performance_all.csv"
+csvfile = open(file_path, mode="w", newline='', encoding='utf-8')
+header = [
+    'data',
+    'model',
+    'prefix_num', 'prefix_source',
+    'beam_size',
+    'sample', 'temperature', 'repeat',
+    'prompt_type',
+    'valid',
+    'acc', 'auc', 'p', 'r', 'F1'
+]
 writer = csv.DictWriter(csvfile, fieldnames=header)
-#
 writer.writeheader()
-for cur_model in models_performance:
+for cur_prompt_info in prompt_info_list:
     writer.writerow({
-        'data': ''.join(data_config),
-        'model': cur_model,
-        'template': models_performance[cur_model]['template'],
-        'prefix-source': models_performance[cur_model]['prefix-source'],
-        'prefix-num': models_performance[cur_model]['prefix-num'],
-        'do_sample': do_sample,
-        'num_beam': num_beams,
-        'temperature': temperature,
+        'data': cur_prompt_info['data'],
+        'model': cur_prompt_info['model'],
+        'prefix_num': cur_prompt_info['prefix_num'],
+        'prefix_source': cur_prompt_info['prefix_source'],
+        'beam_size': cur_prompt_info['beam_size'],
+        'sample': cur_prompt_info['sample'],
+        'temperature': cur_prompt_info['temperature'],
+        'repeat': cur_prompt_info['repeat'],
+        'prompt_type': cur_prompt_info['prompt_type'],
         'valid': '100%',
-        'acc': models_performance[cur_model]['acc'],
-        'auc': models_performance[cur_model]['auc'],
-        'p': models_performance[cur_model]['precision'],
-        'r': models_performance[cur_model]['recall'],
-        'F1': models_performance[cur_model]['f1']
+        'acc': cur_prompt_info['acc'],
+        'auc': cur_prompt_info['auc'],
+        'p': cur_prompt_info['precision'],
+        'r': cur_prompt_info['recall'],
+        'F1': cur_prompt_info['f1']
     })
+print(f"结果输出到{file_path}")
+# =============================================================================
+result = {}
+for cur_prompt_info in prompt_info_list:
+    model = cur_prompt_info['model']
+    prompt_type = cur_prompt_info['prompt_type']
+    prefix_num = cur_prompt_info['prefix_num']
+    sample = cur_prompt_info['sample']
+    f1 = f"{round(cur_prompt_info['f1'], 4):.4f}"
+    if model not in result:
+        result[model] = {}
+    if prompt_type not in result[model]:
+        result[model][prompt_type] = {}
+    if prefix_num not in result[model][prompt_type]:
+        result[model][prompt_type][prefix_num] = ""
+    cur_value = result[model][prompt_type][prefix_num]
+    if sample == "do":
+        cur_value = f1 + cur_value
+    elif sample == "no":
+        cur_value = cur_value + f" [{f1}]"
+    else:
+        raise RuntimeError("invalid value of sample")
+    result[model][prompt_type][prefix_num] = cur_value
+file_path = results_path + "/performance_model.csv"
+csvfile = open(file_path, mode="w", newline='', encoding='utf-8')
+header = ['prompt_type', '0-shot', '2-shot', '4-shot', '10-shot']
+writer = csv.DictWriter(csvfile, fieldnames=header)
+writer.writeheader()
+for model in result.keys():
+    writer.writerow({'prompt_type': model})
+    for prompt_type in result[model]:
+        row = {
+            'prompt_type': prompt_type,
+            '0-shot': '-' if '0' not in result[model][prompt_type] else result[model][prompt_type]['0'],
+            '2-shot': '-' if '2' not in result[model][prompt_type] else result[model][prompt_type]['2'],
+            '4-shot': '-' if '4' not in result[model][prompt_type] else result[model][prompt_type]['4'],
+            '10-shot': '-' if '10' not in result[model][prompt_type] else result[model][prompt_type]['10']
+        }
+        writer.writerow(row)
 print(f"结果输出到{file_path}")
