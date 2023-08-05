d71cca4c8cea95c731bd3ac23e5f9439cf04bd00
XuYang <xuyangng_mail@sina.cn>
2023-08-05 22:41:07 +0800

    3.pred.py - 重命名，把原理的generate_prompt.py改成pred.py - 功能：保存代码到输出路径 - 功能：repeat设置 - 功能：mention pair中记录（做对了多少，有多少有效值，重复实现了几次） - 功能：保存到csv

--- Models2/src/3.pred.py
+++ Models2/src/3.pred.py
@@ -5,6 +5,8 @@ import _pickle as cPickle
 # from typing import Dict, List, Tuple, Union
 import logging
 import time
+import shutil
+import pandas as pd
 # 本地库
 from classes import Corpus, Topic, Document, Sentence, Token, EventMention, EntityMention, MentionData
 from template import templates_list
@@ -12,7 +14,8 @@ from template import templates_list
 
 # config
 config_dict = {
-    "corpus_and_mention_pair_path": r"E:\ProgramCode\WhatGPTKnowsAboutWhoIsWho\WhatGPTKnowsAboutWhoIsWho-main\Models2\data\extract_mention_pairs_from_test_data\strategy_3_corpus_and_mention_pairs",
+    "corpus_path": r"E:\ProgramCode\WhatGPTKnowsAboutWhoIsWho\WhatGPTKnowsAboutWhoIsWho-main\Models2\data\1.read_corpus\test_data",
+    "mention_pairs_path": r"E:\ProgramCode\WhatGPTKnowsAboutWhoIsWho\WhatGPTKnowsAboutWhoIsWho-main\Models2\data\2.extract_mention_pairs_from_test_data\test(strategy3).mp",
     "output_path": r"E:\ProgramCode\WhatGPTKnowsAboutWhoIsWho\WhatGPTKnowsAboutWhoIsWho-main\Models2\output",
     "models": {
         # "GPT2": {
@@ -27,11 +30,14 @@ config_dict = {
             },  # or None if you do not use system message
             "temperature": 0,
             "do_sample": False,
+            "repeat": 1,
         },
     },
     "templates": ["14DAM"],
     "data": ["36_ecb","36_ecbplus"]
 }
+
+# 临时代码，用于清空输出路径
 for file in os.listdir(config_dict["output_path"]):
     os.remove(os.path.join(config_dict["output_path"], file))
 
@@ -51,6 +57,10 @@ if not os.path.exists(config_dict["output_path"]):
     os.makedirs(config_dict["output_path"])
 elif len(os.listdir(config_dict["output_path"])) > 1:  # 大于1是因为上边配置logging的时候就建立的log.txt这个文件
     input("output dir is not empty, press ENTER to continue.")
+
+
+# save this file itself
+shutil.copy(os.path.abspath(__file__), config_dict["output_path"])
 
 
 def get_context(mention1, mention2, context_level="sentence", mark_mention=False, selected_sentence_only=True):
@@ -201,80 +211,86 @@ def create_model(model_name):
 def process_a_mention_pair(model_name, template_id, mention1, mention2):
     model_config = config_dict["models"][model_name]
     prompt = make_prompt(template_id, mention1, mention2)
-    if model_name == "GPT2":
-        tokenizer = model_config["model_components"][0]
-        model = model_config["model_components"][1]
-        inputs = tokenizer(prompt, return_tensors="pt")
-        outputs = model.generate(
-            **inputs, max_new_tokens=1,
-            do_sample=model_config["do_sample"],
-            num_beams=model_config["num_beams"],
-            temperature=model_config["temperature"]
-        )
-        outputs = outputs.tolist()[0]
-        pred = outputs[len(inputs.input_ids[0]):]
-        pred = tokenizer.decode(pred, skip_special_tokens=True)
-        return pred
-    elif model_name == "ChatGLM6B":
-        return True
-    elif model_name == "ChatGPT3.5":
-        import openai
-        #
-        messages = model_config["model_components"]
-        #
-        global pred_index
-        pred_index += 1
-        print(f"{pred_index}##############\n{prompt}\n")
-        prompt_log_file = config_dict["prompt_log_file"]
-        prompt_log_file.write(f"{pred_index}##############\n{prompt}\n")
-        pred = ""
-        cur_time = time.time()
-        while True:
-            try:
-                openai.api_key = os.getenv("OPENAI_API_KEY")
-                openai.proxy = "127.0.0.1:15732"
-                m = []
-                m = m + messages
-                m.append({
-                    "role": "user",
-                    "content": prompt
-                })
-                # if model_config["do_sample"]:  # do sample
-                #     r = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=m, temperature=model_config["temperature"], max_tokens=1)
-                # else:  # no sample
-                #     r = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=m, temperature=0.0, max_tokens=1)
-                # pred = r["choices"][0]["message"]["content"]
-                pred = "Yes"
-                print("success", time.time())
-                break
-            except openai.OpenAIError as e:
-                # 如果是访问过快就等等
-                if re.match(r"Rate limit reached for default-gpt-3.5-turbo in organization org-ooYBZEO3oP4YJO7LGYUbhNjf on requests per min. Limit: 3 / min.", e._message) is not None:
-                    print("rate limit", time.time())
-                    time.sleep(10)
-                # 如果是bad gateway就重试
-                elif "Bad gateway" in e._message:
-                    print("bad gateway", time.time())
-                    if (time.time() - cur_time) < 1:
-                        input("two error in 1 second")
-                    cur_time = time.time()
-                    pass
-                # 如果是The server is overloaded or not ready yet.就重试
-                elif "The server is overloaded or not ready yet" in e._message:
-                    print("server overloaded", time.time())
-                    time.sleep(1)
-                # 如果是其他问题就暂停
-                else:
-                    print(e._message)
-                    """
-                    ipt = input("Press Enter to request again... Press 'Code' to debug...")
-                    if ipt == "Code":
-                        while True:
-                            command = input("input your debug command:")
-                            o = eval(command)
-                            print(o)
-                    """
-        return pred
+    true_num, validated_num, repeat_num = 0, 0, model_config["repeat"]
+    ground_truth = (mention1.gold_tag == mention2.gold_tag)
+    for repeat_index in range(repeat_num):
+        if model_name == "GPT2":
+            tokenizer = model_config["model_components"][0]
+            model = model_config["model_components"][1]
+            inputs = tokenizer(prompt, return_tensors="pt")
+            outputs = model.generate(
+                **inputs, max_new_tokens=1,
+                do_sample=model_config["do_sample"],
+                num_beams=model_config["num_beams"],
+                temperature=model_config["temperature"]
+            )
+            outputs = outputs.tolist()[0]
+            pred = outputs[len(inputs.input_ids[0]):]
+            pred = tokenizer.decode(pred, skip_special_tokens=True)
+        elif model_name == "ChatGLM6B":
+            pred = True
+        elif model_name == "ChatGPT3.5":
+            import openai
+            #
+            messages = model_config["model_components"]
+            #
+            global pred_index
+            pred_index += 1
+            print(f"{pred_index}##############\n{prompt}\n")
+            prompt_log_file = config_dict["prompt_log_file"]
+            prompt_log_file.write(f"{pred_index}##############\n{prompt}\n")
+            pred = ""
+            cur_time = time.time()
+            while True:
+                try:
+                    openai.api_key = os.getenv("OPENAI_API_KEY")
+                    openai.proxy = "127.0.0.1:15732"
+                    m = []
+                    m = m + messages
+                    m.append({
+                        "role": "user",
+                        "content": prompt
+                    })
+                    # if model_config["do_sample"]:  # do sample
+                    #     r = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=m, temperature=model_config["temperature"], max_tokens=1)
+                    # else:  # no sample
+                    #     r = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=m, temperature=0.0, max_tokens=1)
+                    # pred = r["choices"][0]["message"]["content"]
+                    pred = "Yes"
+                    pred = True
+                    validated_num += 1
+                    if pred == ground_truth:
+                        true_num += 1
+                    print("success", time.time())
+                    break
+                except openai.OpenAIError as e:
+                    # 如果是访问过快就等等
+                    if re.match(r"Rate limit reached for default-gpt-3.5-turbo in organization org-ooYBZEO3oP4YJO7LGYUbhNjf on requests per min. Limit: 3 / min.", e._message) is not None:
+                        print("rate limit", time.time())
+                        time.sleep(10)
+                    # 如果是bad gateway就重试
+                    elif "Bad gateway" in e._message:
+                        print("bad gateway", time.time())
+                        if (time.time() - cur_time) < 1:
+                            input("two error in 1 second")
+                        cur_time = time.time()
+                        pass
+                    # 如果是The server is overloaded or not ready yet.就重试
+                    elif "The server is overloaded or not ready yet" in e._message:
+                        print("server overloaded", time.time())
+                        time.sleep(1)
+                    # 如果是其他问题就暂停
+                    else:
+                        print(e._message)
+                        """
+                        ipt = input("Press Enter to request again... Press 'Code' to debug...")
+                        if ipt == "Code":
+                            while True:
+                                command = input("input your debug command:")
+                                o = eval(command)
+                                print(o)
+                        """
+    return [true_num, validated_num, repeat_num]
 
 
 def predicate(model_name, template_id, mention_pairs):
@@ -311,12 +327,21 @@ def predicate(model_name, template_id, mention_pairs):
     #
     print("\n")
 
+
 def main():
+    # 代码对每个model和每个template分别进行实验
     for cur_model_name, cur_model_config in config_dict["models"].items():
         for cur_template_id in config_dict["templates"]:
+            # 针对当前model和当前template实验一次
+            logging.info(f"Start {cur_model_name} model with template {cur_template_id}============================")
+            print(f"Start {cur_model_name} model with template {cur_template_id}============================")
             # read corpus
-            with open(config_dict["corpus_and_mention_pair_path"], 'rb') as f:
-                corpus, mention_pairs = cPickle.load(f)
+            with open(config_dict["corpus_path"], 'rb') as f:
+                corpus = cPickle.load(f)
+            with open(config_dict["mention_pairs_path"], 'rb') as f:
+                mention_pairs = cPickle.load(f)
+            #
+            strategy_id = re.search("test\(strategy([0-4])\).mp", config_dict["mention_pairs_path"]).groups()[0]
             # select a subset of the whole corpus
             topics_list = list(corpus.topics.keys())  # TODO: 知识点总结。如果不加list()，那么topics_list是会随着corpus的改变而改变的。
             for cur_topic_id in topics_list:
@@ -329,7 +354,7 @@ def main():
             config_dict["corpus"] = corpus
             config_dict["mention_pairs"] = mention_pairs
             #
-            config_dict["file_name"] = f"{cur_model_name}_{config_dict['data']}_t{cur_template_id}_s0_b1_noSample"
+            config_dict["file_name"] = f"{config_dict['data']}(strategy{strategy_id})_{cur_model_name}_t{cur_template_id}_s0_b1_noSample"
             config_dict["corpus_path"] = f"{config_dict['output_path']}/{config_dict['file_name']}.pkl"
             config_dict["prompt_log_file_path"] = f"{config_dict['output_path']}/{config_dict['file_name']}.promptlog"
             config_dict["prompt_log_file"] = open(config_dict["prompt_log_file_path"], mode="w", encoding="utf8")
@@ -346,12 +371,31 @@ def main():
                     cPickle.dump(corpus, f)
                     print(f"corpus obj of {config_dict['data']} saved in {corpus_path}")
             os.path.exists(corpus_path)
+            # 保存
             mention_pairs_path = f"{config_dict['output_path']}\{config_dict['file_name']}.mp"
             if os.path.exists(mention_pairs_path):
                 raise RuntimeError("重复的保存")
             with open(mention_pairs_path, 'wb') as f:
                 cPickle.dump(mention_pairs, f)
                 print(f"mention pairs list and pred result under cur config saved in {corpus_path}")
+            # 保存
+            mention_pairs_path = f"{config_dict['output_path']}\{config_dict['file_name']}.csv"
+            csv_list = []
+            csv_list.append(["topic", "m1_doc", "m1_sent", "m1_str", "m2_doc", "m2_sent", "m2_str", "wd/cd", "seq", "corefer?", cur_template_id])
+            for cur_topic_id in mention_pairs.keys():
+                for mention1, mention2, [true_num, validated_num, repeat_num] in mention_pairs[cur_topic_id]:
+                    if_wd = (mention1.doc_id == mention2.doc_id)
+                    csv_list.append([
+                        cur_topic_id,
+                        mention1.doc_id, mention1.sent_id, mention1.mention_str,
+                        mention2.doc_id, mention2.sent_id, mention2.mention_str,
+                        "wd" if if_wd else "cd",
+                        abs(mention1.sent_id - mention2.sent_id) if if_wd else None,
+                        mention1.gold_tag == mention2.gold_tag,
+                        [true_num, validated_num, repeat_num]
+                    ])
+            csv_df = pd.DataFrame(csv_list)
+            csv_df.to_csv(mention_pairs_path, mode="a", encoding="utf-8", index=False, header=False)
             # 销毁模型（释放内存）
             del config_dict["models"][cur_model_name]["model_components"]
             #
