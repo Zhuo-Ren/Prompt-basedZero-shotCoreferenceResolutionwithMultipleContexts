24f36696b29b92a7d9d78e8eaada4c7e2a274532
Zhuo-Ren <52392665+Zhuo-Ren@users.noreply.github.com>
2023-08-21 22:01:04 +0800

    功能：实现wd coref

--- Models2/src/456.py
+++ Models2/src/456.py
@@ -21,7 +21,7 @@ import csv
 from classes import Corpus, Topic, Document, Sentence, Token, Mention, EventMention, EntityMention, MentionData
 # 本地库
 from th4_mention_pairs_scorer import get_cmp_or_csv_files, get_score_from_csv, save_mention_pair_scores_into_csv_in_list_format, save_mention_pair_scores_into_csv_in_table_format, get_experiment_settings
-from th5_clustering import adapter_of_mention_pairs, clustering, remove_unselected_mention, check_whether_all_mentions_are_clustered
+from th5_clustering import adapter_of_mention_pairs, cd_clustering, wd_clustering, remove_unselected_mention, check_whether_all_mentions_are_clustered
 from th6_clustering_scorer import coreference_scorer, save_clustering_scores_into_csv_in_list_format, save_clustering_scores_into_csv_in_table_format
 
 
@@ -67,8 +67,8 @@ def main():
               f"===Mention Pair:{os.path.basename(cur_experiment_path)}===\n"
               f"{'='*(19+len(os.path.basename(cur_experiment_path)))}")
         logging.info(f"\n{'='*(19+len(os.path.basename(cur_experiment_path)))}\n"
-              f"===Mention Pair:{os.path.basename(cur_experiment_path)}===\n"
-              f"{'='*(19+len(os.path.basename(cur_experiment_path)))}")
+                     f"===Mention Pair:{os.path.basename(cur_experiment_path)}===\n"
+                     f"{'='*(19+len(os.path.basename(cur_experiment_path)))}")
         #
         cur_csv_path = f"{cur_experiment_path}.csv"
         # 保存
@@ -88,15 +88,86 @@ def main():
         info.update(settings)
         info.update(scores)
         experiments_scores.append(info)
-    print(f"\n{'='*(19+len(os.path.basename(cur_experiment_path)))}\n"
+    print(f"\n=======================\n"
           f"===Mention Pair:整合===\n"
-          f"{'='*(19+len(os.path.basename(cur_experiment_path)))}")
-    logging.info(f"\n{'='*(19+len(os.path.basename(cur_experiment_path)))}\n"
-          f"===Mention Pair:整合===\n"
-          f"{'='*(19+len(os.path.basename(cur_experiment_path)))}")
+          f"=======================")
+    logging.info(f"\n=======================\n"
+                 f"===Mention Pair:整合===\n"
+                 f"=======================")
     save_mention_pair_scores_into_csv_in_list_format(experiments_scores, output_path=config_dict["output_path"])
     save_mention_pair_scores_into_csv_in_table_format(experiments_scores, output_path=config_dict["output_path"])
-    # 2.cd coref部分
+    # 2.wd coref部分
+    experiments_scores = []
+    for cur_experiment_path in experiment_path_list:
+        print(f"\n{'=' * (19 + len(os.path.basename(cur_experiment_path)))}\n"
+              f"===CD Clustering:{os.path.basename(cur_experiment_path)}===\n"
+              f"{'=' * (19 + len(os.path.basename(cur_experiment_path)))}")
+        logging.info(f"\n{'=' * (19 + len(os.path.basename(cur_experiment_path)))}\n"
+                     f"===CD Clustering:{os.path.basename(cur_experiment_path)}===\n"
+                     f"{'=' * (19 + len(os.path.basename(cur_experiment_path)))}")
+        #
+        cur_cmp_path = f"{cur_experiment_path}.c_mp"
+        # 抽取配置
+        settings = get_experiment_settings(cur_cmp_path)
+        # 读取
+        with open(cur_cmp_path, 'rb') as f:
+            corpus, mention_pairs = cPickle.load(f)
+        # wd聚类
+        for cur_topic_id, cur_topic_mp in mention_pairs.items():
+            adapter_of_mention_pairs(cur_topic_mp)
+            #
+            n = int(re.search("([0-9]*)_ecb", cur_topic_id).groups()[0])
+            p = 1 if "plus" in cur_topic_id else 0
+            prefix = n * 100000000 + p * 1000000
+            #
+            wd_clustering(prefix=prefix, mention_pairs_list=cur_topic_mp)
+            #
+            del cur_topic_id, cur_topic_mp, n, p, prefix
+        remove_unselected_mention(corpus)
+        check_whether_all_mentions_are_clustered(corpus)
+        # 保存聚类结果
+        path = os.path.join(config_dict['output_path'],
+                            f"{os.path.basename(cur_experiment_path)}.wd_clustering.clustered_corpus")
+        with open(path, 'wb') as f:
+            cPickle.dump(corpus, f)
+        print(f"OUTPUT: corpus and wd clustering result under cur config saved in {path}")
+        logging.info(f"OUTPUT: corpus and wd clustering result under cur config saved in {path}")
+        # 打分
+        for cur_topic_id, cur_topic in corpus.topics.items():
+            for cur_doc_id, cur_doc in cur_topic.docs.items():
+                for cur_sent_id, cur_sent in cur_doc.sentences.items():
+                    for cur_mention in cur_sent.gold_entity_mentions + cur_sent.gold_event_mentions:
+                        cur_mention.gold_tag = f"{cur_doc_id}-{cur_mention.gold_tag}"
+                        del cur_mention
+                    del cur_sent_id
+                del cur_doc_id, cur_doc
+            del cur_topic_id, cur_topic
+        prefix = f"{os.path.basename(cur_experiment_path)}.wd_clustering"
+        scores = coreference_scorer(corpus, config_dict["output_path"], output_prefix=prefix)
+        # 保存
+        path = os.path.join(config_dict["output_path"], f"{prefix}.scores")
+        with open(path, 'w', encoding="utf8") as f:
+            f.writelines([f"{k}: {v}\n" for k, v in scores.items()])
+        print(f"OUTPUT: clustering scores saved in {path}")
+        logging.info(f"OUTPUT: clustering scores saved in {path}")
+        # 分数整合
+        info = {}
+        info.update(settings)
+        info.update(scores)
+        experiments_scores.append(info)
+    print(f"\n===================\n"
+          f"===WD Coref:整合===\n"
+          f"===================")
+    logging.info(f"\n===================\n"
+                 f"===WD Coref:整合===\n"
+                 f"===================")
+    save_clustering_scores_into_csv_in_list_format(experiments_scores,
+                                                   output_path=config_dict["output_path"],
+                                                   suffix="scores_wd_clustering_list.csv")
+    save_clustering_scores_into_csv_in_table_format(experiments_scores,
+                                                    output_path=config_dict["output_path"],
+                                                    suffix="scores_wd_clustering_table.csv")
+    # 3.cd coref部分
     experiments_scores = []
     for cur_experiment_path in experiment_path_list:
         print(f"\n{'='*(19+len(os.path.basename(cur_experiment_path)))}\n"
@@ -120,37 +191,42 @@ def main():
             p = 1 if "plus" in cur_topic_id else 0
             prefix = n*100000000 + p*1000000
             #
-            clustering(prefix=prefix, mention_pairs_list=cur_topic_mp)
+            cd_clustering(prefix=prefix, mention_pairs_list=cur_topic_mp)
         remove_unselected_mention(corpus)
         check_whether_all_mentions_are_clustered(corpus)
         # 保存聚类结果
-        path = os.path.join(config_dict['output_path'], f"{os.path.basename(cur_experiment_path)}.clustering1.clustered_corpus")
+        path = os.path.join(config_dict['output_path'], f"{os.path.basename(cur_experiment_path)}.cd_clustering.clustered_corpus")
         with open(path, 'wb') as f:
             cPickle.dump(corpus, f)
-        print(f"OUTPUT: corpus and clustering result under cur config saved in {path}")
-        logging.info(f"OUTPUT: corpus and clustering result under cur config saved in {path}")
+        print(f"OUTPUT: corpus and cd clustering result under cur config saved in {path}")
+        logging.info(f"OUTPUT: corpus and cd clustering result under cur config saved in {path}")
         # 打分
-        prefix = f"{os.path.basename(cur_experiment_path)}.clustering1"
+        prefix = f"{os.path.basename(cur_experiment_path)}.cd_clustering"
         scores = coreference_scorer(corpus, config_dict["output_path"], output_prefix=prefix)
         # 保存
         path = os.path.join(config_dict["output_path"], f"{prefix}.scores")
         with open(path, 'w', encoding="utf8") as f:
             f.writelines([f"{k}: {v}\n" for k, v in scores.items()])
-        print(f"OUTPUT: clustering scores saved in {path}")
-        logging.info(f"OUTPUT: clustering scores saved in {path}")
+        print(f"OUTPUT: cd clustering scores saved in {path}")
+        logging.info(f"OUTPUT: cd clustering scores saved in {path}")
         # 分数整合
         info = {}
         info.update(settings)
         info.update(scores)
         experiments_scores.append(info)
-    print(f"\n{'='*(19+len(os.path.basename(cur_experiment_path)))}\n"
+    print(f"\n===================\n"
           f"===CD Coref:整合===\n"
-          f"{'='*(19+len(os.path.basename(cur_experiment_path)))}")
-    logging.info(f"\n{'='*(19+len(os.path.basename(cur_experiment_path)))}\n"
+          f"===================")
+    logging.info(f"\n===================\n"
                  f"===CD Coref:整合===\n"
-                 f"{'='*(19+len(os.path.basename(cur_experiment_path)))}")
-    save_clustering_scores_into_csv_in_list_format(experiments_scores, output_path=config_dict["output_path"])
-    save_clustering_scores_into_csv_in_table_format(experiments_scores, output_path=config_dict["output_path"])
+                 f"===================")
+    save_clustering_scores_into_csv_in_list_format(experiments_scores,
+                                                   output_path=config_dict["output_path"],
+                                                   suffix="scores_cd_clustering_list.csv")
+    save_clustering_scores_into_csv_in_table_format(experiments_scores,
+                                                    output_path=config_dict["output_path"],
+                                                    suffix="scores_cd_clustering_table.csv")
+
 
 if __name__ == '__main__':
     main()
