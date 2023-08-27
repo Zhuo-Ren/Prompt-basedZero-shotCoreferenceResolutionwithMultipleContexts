013a827477cbcfee941ed557fce91d7faa739dac
Zhuo-Ren <52392665+Zhuo-Ren@users.noreply.github.com>
2023-08-27 23:09:22 +0800

    添加新的template

--- Models2/src/template.py
+++ Models2/src/template.py
@@ -27,7 +27,7 @@ Answer: <Yes or No>
 Text:\"\"\"[S_CONTEXT] \"\"\"
 Mention1: [MENTION1]
 Mention2: [MENTION2]
-Answer:""",
+Answer:""",  # 和7相比，Text:后边没有空格了
     "9SAU": """Please determine whether two mentions in a given text are co-referred. Two mention are co-referred only when they are refer to the identical thing. Note that if a mention is a side, a part, a view or a aspect of aonther mention, the two mentions don't co-referred. 
 Use the following format:
 Text: <the given text>
@@ -38,7 +38,7 @@ Answer: <Yes or No>
 Text:\"\"\"[S_CONTEXT] \"\"\"
 Mention1:" [MENTION1] "
 Mention2:" [MENTION2] "
-Answer:""",
+Answer:""",  # 和8相比，Mention1和Mention2后边加了引号
     "10SAU": """Please determine whether two mentions in a given text are co-referred. Two mention are co-referred only when they are refer to the identical thing. Note that if a mention is a side, a part, a view or a aspect of aonther mention, the two mentions don't co-referred. 
 Use the following format:
 Text: <the given text>
@@ -49,7 +49,7 @@ Answer: <Yes or No>
 Text: < [S_CONTEXT] >
 Mention1: < [MENTION1] >
 Mention2: < [MENTION2] >
-Answer:""",
+Answer:""",  # 和10相比，引号变成了尖括号
     "11SAU": """Please determine whether two mentions in a given text are co-referred. Two mention are co-referred only when they are refer to the identical thing. Note that if a mention is a side, a part, a view or a aspect of aonther mention, the two mentions don't co-referred. 
 Use the following format:
 Text: < the given text >
@@ -60,7 +60,7 @@ Answer: < Yes or No >
 Text: < [S_CONTEXT] >
 Mention1: < [MENTION1] >
 Mention2: < [MENTION2] >
-Answer:""",
+Answer:""",  # 和10相比，format中的尖括号前后添加了空格。
     "12SAU": """Please determine whether two mentions in a given text are co-referred. Two mention are co-referred only when they are refer to the identical thing. Note that if a mention is a side, a part, a view or a aspect of aonther mention, the two mentions don't co-referred.
 Use the following format:
 Text: < the given text >
@@ -96,5 +96,65 @@ Text: < the given messages with two mentions marked >
 Answer: < Yes or No, why >
 
 Text: < [D_CONTEXT_marked] >
-Answer:"""
+Answer:""",  # 和15相比，优化了解释语
+    "17MAU": """Please determine whether two mentions in a given text are co-referred. Two mention are co-referred only when they are refer to the identical thing. Note that if a mention is a side, a part, a view or a aspect of aonther mention, the two mentions don't co-referred.
+Use the following format:
+Text: < the given text >
+Mention1: < one mention in the given text >
+Mention2: < another mention in the given text >
+Answer: < Yes or No, why >
+
+Text: < [M_CONTEXT] >
+Mention1: < [MENTION1] >
+Mention2: < [MENTION2] >
+Answer:""",  # 和13相同，只不过S_CONTEXT改为了M_CONTEXT
+    "18MAU": """Please determine whether two mentions in a given text are co-referred. Two mention are co-referred only when they are refer to the identical entity or event. Note that if a mention is a side, a part, a view or a aspect of aonther mention, the two mentions don't co-referred. Note that entity mention and event mention can not be coreferred.
+Use the following format:
+Text: < the given text >
+Mention1: < one mention in the given text >
+Mention2: < another mention in the given text >
+Answer: < Yes or No, why >
+
+Text: < [M_CONTEXT] >
+Mention1: < [MENTION1] >
+Mention2: < [MENTION2] >
+Answer:""",  # 相比17增加了任务描述：①thing →  entity or event；②Note that entity mention and event mention can not be coreferred.
+    "19SAU": """Please determine whether two mentions in a given text are co-referred. Two mention are co-referred only when they are refer to the identical entity or event. Note that if a mention is a side, a part, a view or a aspect of aonther mention, the two mentions don't co-referred. Note that entity mention and event mention can not be coreferred.
+Use the following format:
+Text: < the given text >
+Mention1: < one mention in the given text >
+Mention2: < another mention in the given text >
+Answer: < Yes or No, why >
+
+Text: < [S_CONTEXT] >
+Mention1: < [MENTION1] >
+Mention2: < [MENTION2] >
+Answer:""",  # 相比18，只是改为了S_CONTEXT
+    "20MAU": "'[M_CONTEXT]' In previous sentences, does '[MENTION2]' refer to '[MENTION1]'? Yes or no?",  # 相比于0SAU，只是改为了M_CONTEXT
+    "21MAU": "'[M_CONTEXT]' Here, by '[MENTION2]' they mean '[MENTION1]'? Yes or no?",  # 相比于1SAU，只是改为了M_CONTEXT
+    "22MAU": "'[M_CONTEXT]' Here, does '[MENTION2]' stand for '[MENTION1]'? Yes or no? ",  # 相比于2SAU，只是改为了M_CONTEXT
+    "23MAU": "'[M_CONTEXT]' In the passage above, can '[MENTION2]' be replaced by '[MENTION1]'? Yes or no?",  # 相比于3SAU，只是改为了M_CONTEXT
+    "24MAU": "'[M_CONTEXT]' I think '[MENTION2]' means '[MENTION1]'. Yes or no?",  # 相比于4SAU，只是改为了M_CONTEXT
+    "25DAU": """Please determine whether two mentions in a given text are co-referred. Two mention are co-referred only when they are refer to the identical thing. Note that if a mention is a side, a part, a view or a aspect of aonther mention, the two mentions don't co-referred.
+Use the following format:
+Text: < the given text >
+Mention1: < one mention in the given text >
+Mention2: < another mention in the given text >
+Answer: < Yes or No, why >
+
+Text: < [D_CONTEXT] >
+Mention1: < [MENTION1] >
+Mention2: < [MENTION2] >
+Answer:""",  # 和17相同，只不过改为了D_CONTEXT
+    "26DAU": """Please determine whether two mentions in a given text are co-referred. Two mention are co-referred only when they are refer to the identical entity or event. Note that if a mention is a side, a part, a view or a aspect of aonther mention, the two mentions don't co-referred. Note that entity mention and event mention can not be coreferred.
+Use the following format:
+Text: < the given text >
+Mention1: < one mention in the given text >
+Mention2: < another mention in the given text >
+Answer: < Yes or No, why >
+
+Text: < [D_CONTEXT] >
+Mention1: < [MENTION1] >
+Mention2: < [MENTION2] >
+Answer:""",  # 和18相同，只不过改为了D_CONTEXT
 }
